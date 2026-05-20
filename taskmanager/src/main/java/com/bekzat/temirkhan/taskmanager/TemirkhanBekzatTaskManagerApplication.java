aee710751ecd16e9d2cd60ab39d74e2322889582
Bekzat <temirkhanb00@gmail.com>
2026-05-20 06:41:32 +0500

    Add async methods: notification and report generation

---
+++ taskmanager/src/main/java/com/bekzat/temirkhan/taskmanager/TemirkhanBekzatTaskManagerApplication.java
@@ -0,0 +1,12 @@
+package com.bekzat.temirkhan.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemirkhanBekzatTaskManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemirkhanBekzatTaskManagerApplication.class, args);
+    }
+}
